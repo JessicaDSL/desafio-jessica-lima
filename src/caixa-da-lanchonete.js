7c6d95f5e003accf832d61af33f9493e45799b7b
Jessica Lima <jessikurt@gmail.com>
2023-08-16 22:08:36 -0300

    feat: creat const for menu

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,18 @@
+const cardapio = {
+  cafe: 3.0,
+  chantily: 1.5,
+  suco: 6.2,
+  sanduiche: 6.5,
+  queijo: 2,
+  salgado: 7.25,
+  combo1: 9.5,
+  combo2: 7.5,
+};
+
 class CaixaDaLanchonete {
-
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
-    }
-
+  calcularValorDaCompra(metodoDePagamento, itens) {
+    return "";
+  }
 }
 
 export { CaixaDaLanchonete };
